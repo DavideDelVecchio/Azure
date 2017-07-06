43f8a17e435d0cc25ad794abd643459c51ea8fcc
Davide Del Vecchio <dadelve@microsoft.com>
2017-07-06 07:56:38 +0200

    chenges applied to use realmd instead of samba to join the machine in centos 7

--- DomainJoinedLinuxVMOnAzure/scripts/initialize-node.sh
+++ DomainJoinedLinuxVMOnAzure/scripts/initialize-node.sh
@@ -11,6 +11,7 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
+
 ADDNS=$1
 PDC=$2
 BDC=$3
@@ -77,63 +78,31 @@ setenforce 0
 sed -i 's^SELINUX=enforcing^SELINUX=disabled^g' /etc/selinux/config
 
 # Install and configure domain join packages
-yum install -y ntp
-yum -y remove samba-client
-yum -y remove samba-common
+yum -y install realmd
 yum -y install sssd
 yum -y install sssd-client
-yum -y install krb5-workstation
-yum -y install samba4
-yum -y install openldap-clients
+yum -y install krb5-workstation krb5-libs
 yum -y install policycoreutils-python
 
 os=""
 release=""
 findOsVersion
 
-# in CentOS7, tell NetworkManager not to overwrite /etc/resolv.conf
-if [ "$major_release" = "7" ]; then
-    cp -f nwnodns.conf /etc/NetworkManager/conf.d/
-    systemctl restart NetworkManager.service
-fi
 
-cp -f resolv.conf /etc/resolv.conf
-replace_ad_params /etc/resolv.conf
-cp -f krb5.conf /etc/krb5.conf
-replace_ad_params /etc/krb5.conf
-cp -f smb.conf /etc/samba/smb.conf
-replace_ad_params /etc/samba/smb.conf
-cp -f sssd.conf /etc/sssd/sssd.conf
-replace_ad_params /etc/sssd/sssd.conf
-cp -f ntp.conf /etc/ntp.conf
-replace_ad_params /etc/ntp.conf
-sed -i "s/session.*pam_oddjob_mkhomedir.so.*/session     optional      pam_oddjob_mkhomedir.so skel=\/etc\/skel umask=0077/" /etc/pam.d/system-auth
-
-# in CentOS6, prevent /etc/resolv.conf from being overwritten
-cat > /etc/dhcp/dhclient-enter-hooks << EOF
-#!/bin/sh
-make_resolv_conf() {
-echo "do not change resolv.conf"
-}
-EOF
-chmod a+x /etc/dhcp/dhclient-enter-hooks
-
-chmod 600 /etc/sssd/sssd.conf
-service ntpd start
-chkconfig ntpd on
-service smb start
-chkconfig smb on
 
 # Join domain, must join domain first, otherwise sssd won't start
 shortHostName=`hostname -s`
 hostname ${shortHostName}.${ADDNS}
 n=0
+UPPERDOMAIN=$(echo ${ADDNS} | tr 'a-z' 'A-Z' )
+echo "value for Upper Domain :" ${UPPERDOMAIN}
+echo  ${DOMAINADMINPWD} | kinit ${DOMAINADMINUSER}@${UPPERDOMAIN}
 until [ $n -ge 4 ]
 do
   if [ ! -z "$ADOUPATH" ]; then
-    net ads join createcomputer="$ADOUPATH" -U${DOMAINADMINUSER}@${ADDNS}%${DOMAINADMINPWD}  
+    realm join ${UPPERDOMAIN} --computer-ou=${ADOUPATH} -U ${DOMAINADMINUSER}@${UPPERDOMAIN} --verbose 
   else
-    net ads join -U${DOMAINADMINUSER}@${ADDNS}%${DOMAINADMINPWD}  
+    realm join ${ADDNS} -U ${DOMAINADMINUSER}@${UPPERDOMAIN} --verbose
   fi
   result=$?
   [ $result -eq 0 ] && break
@@ -151,3 +120,4 @@ else
   hostname ${shortHostName}
   exit 1
 fi
+
